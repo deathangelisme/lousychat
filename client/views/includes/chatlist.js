060d915de3aecb1dd9ee17741377bfe11eef1f8a
dangel <deathangelisme@gmail.com>
2014-07-07 03:50:52 -0400

    Fix another new chat issue

--- client/views/includes/chatlist.js
+++ client/views/includes/chatlist.js
@@ -1,6 +1,6 @@
 Template.chatlist.helpers({
 	getChatCount: function(chat_sender) {
-		var newchat = Chats.find({sender: chat_sender, unread: true}, {sort: {created_at: -1}}).fetch();
+		var newchat = Chats.find({sender: chat_sender, recipient: Meteor.user().username, unread: true}, {sort: {created_at: -1}}).fetch();
 		if (newchat.length > 0) {
 			// should show the number of new chats per user
 			return '';
